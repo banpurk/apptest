bf4a7da5c51afa98e949fa6bf72739e185ca5bf2
banpurk <43028841+banpurk@users.noreply.github.com>
2020-03-18 02:13:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
     
   def post(self):
+        Compliance = self.request.get("Compliance")
+        Workload = self.request.get("Workload")
         self.response.out.write("Compliance selected: " + Compliance + " Workload selected: " + Workload)
     
 app = webapp2.WSGIApplication([('/', MainPage)], debug= True)
