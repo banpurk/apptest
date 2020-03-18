81ae761da0a0a400e5604ef2f240ba15237430e8
banpurk <43028841+banpurk@users.noreply.github.com>
2020-03-18 01:31:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,20 @@ import webapp2
 html = """
 <html>
         <head>
-                This is a form to pick workload and compliance
+                This is a form to pick compliance and workload 
 
         </head>
                 <body>
-                       <p>Select a compliance from the below options:</p>
-
-        <form id="test" action="datastore" method="post">
-                <fieldset id="group1">
-                        <input type="radio" name="compliance" onclick="myFunction1(this.value)" id="group1" value="GDPR">GDPR<br>
-                        <input type="radio" name="compliance" onclick="myFunction1(this.value)" id="group1" value="PCI">PCI<br>
-                        <input type="radio" name="compliance" onclick="myFunction1(this.value)" id="group1" value="HIPAA">HIPAA<br>
-                </fieldset>
-
-                        <p>Select a workload from the below options:</p>
-                <fieldset id="group2">
-                        <input type="radio" name="workload" onclick="myFunction2(this.value)" id="group2" value="DataWarehouse">DataWarehouse<br>
-                        <input type="radio" name="workload" onclick="myFunction2(this.value)" id="group2" value="DLP">DLP<br>
-                </fieldset>
+        <form method="post">
+                <label for= "Compliance">Compliance:</label>
+                <input name="Compliance" type="text" value=""><br>
+                
+                <label for= "Workload">Workoad:</label>
+                <input name="Workload" type="text" value=""><br>
+                
+                <input name="" type="submit" value="">
         </form>
-        
-        <script>
-        function myFunction1(compliance) {
-          document.getElementById("result").value = compliance;
-        }
-        
-        function myFunction2(workload) {
-          document.getElementById("result").value = workload;
-        }
-        </script>
-                </body>
+        </body>
 </html>
 """
 
