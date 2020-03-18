0092e63c3503a097da78a93b7fd54b796efc289f
banpurk <43028841+banpurk@users.noreply.github.com>
2020-03-18 02:08:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import webapp2
 import os
 import jinja2
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 class MainPage(webapp2.RequestHandler):
   def get(self):
         template_values = {
