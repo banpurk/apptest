327abe78e22bed99a40c5cf74c1d33943507db4a
banpurk <43028841+banpurk@users.noreply.github.com>
2020-03-17 21:21:03 -0700

    Create dshandler.py

---
+++ dshandler.py
@@ -0,0 +1,18 @@
+class Datastore(webapp2.RequestHandler):
+
+    def post(self):
+        print("Hello")
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = Author(
+                    identity=users.get_current_user().user_id(),
+                    email=users.get_current_user().email())
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
