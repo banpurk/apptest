d7945c50256bc71f8a12bca1a118eefa4822b9a2
banpurk <43028841+banpurk@users.noreply.github.com>
2020-04-03 13:00:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class MainPage(webapp2.RequestHandler):
             'Workload': Workload,
         }
       
-        template = JINJA_ENVIRONMENT.get_template('new (2).html')
+        template = JINJA_ENVIRONMENT.get_template('new.html')
         self.response.write(template.render(template_values))
     
    def post(self):
