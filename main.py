f200dd560ff86261ebf47604ed839d17b3396878
banpurk <43028841+banpurk@users.noreply.github.com>
2020-04-03 15:47:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,13 @@ class MainPage(webapp2.RequestHandler):
         entry.compliance = self.request.get("Compliance")
         entry.workload = self.request.get("Workload")
         entry.put()
+        
+class Congrats(webapp2.RequestHandler):
+      
+        template = JINJA_ENVIRONMENT.get_template('congrats.html')
+        self.response.write(template.render(template_values))
     
 app = webapp2.WSGIApplication([('/', Login), 
-                               ('/home', MainPage)], debug= True)
+                               ('/home', MainPage)], 
+                              ('/congrats', Congrats)], debug= True)
   
