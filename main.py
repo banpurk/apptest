2d2d2658407310d239fbd986a43fa113263397b8
banpurk <43028841+banpurk@users.noreply.github.com>
2020-04-16 16:38:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import webapp2
 import os
 import jinja2
 import urllib
+import json
+import cloudstorage
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
@@ -48,10 +50,16 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
     
    def post(self):
-        entry = Entry()
+    '''    entry = Entry()
         entry.compliance = self.request.get("Compliance")
         entry.workload = self.request.get("Workload")
         entry.put()
+        storage_client = storage.Client()'''
+        bucket = storage_client.bucket("divine-engine-270122.appspot.com")
+#       blob = bucket.blob("job-1")
+        filename = '/{}/job-1'.format(bucket)
+        with cloudstorage.open(filename, 'w') as filehandle:
+            filehandle.write(json.dumps({"god": "great"}))
         template = JINJA_ENVIRONMENT.get_template('congrats.html')
         self.response.write(template.render())
 
