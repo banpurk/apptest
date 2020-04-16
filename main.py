c85090e090a3dc2c9ca662cc48191110e0bcfddd
banpurk <43028841+banpurk@users.noreply.github.com>
2020-04-16 16:30:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ import webapp2
 import os
 import jinja2
 import urllib
-import json
-import cloudstorage
 from google.appengine.ext import ndb
 from google.appengine.api import users
-from google.appengine.ext import blobstore
-from google.appengine.ext.webapp import blobstore_handlers
-from google.cloud import storage
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -52,16 +47,15 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('new.html')
         self.response.write(template.render(template_values))
     
-  ''' def post(self):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket("divine-engine-270122.appspot.com")
-#    blob = bucket.blob("job-1")
-    filename = '/{}/job-1'.format(bucket)
-    with cloudstorage.open(filename, 'w') as filehandle:
-            filehandle.write(json.dumps({"god": "great"}))
-    
-    template = JINJA_ENVIRONMENT.get_template('congrats.html')
-    self.response.write(template.render())'''
+   def post(self):
+        entry = Entry()
+        entry.compliance = self.request.get("Compliance")
+        entry.workload = self.request.get("Workload")
+        entry.put()
+        template = JINJA_ENVIRONMENT.get_template('congrats.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([('/', Login), 
                                ('/home', MainPage)], debug= True)
+               #                ('/home#',Login)
+  
